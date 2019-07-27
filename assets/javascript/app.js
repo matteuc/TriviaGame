55db8574ac45978865b0fc31dd9ad852f6190ec6
matteuc <ymc006@ucsd.edu>
2019-07-27 00:29:56 -0700

    working, need to add: end page, stylings for start page, actual questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var numCorrect, numIncorrect;
+var numCorrect, numIncorrect, numMissed;
 var timerRunning = false;
 var timerId;
 var time = 0;
@@ -6,25 +6,41 @@ var time = 0;
 var questions = [];
 var questionIndex = 0;
 
+var correctSounds = ["./assets/sounds/jake-peralta-chills-literal-chills.mp3", "./assets/sounds/brooklyn-99-bingpot.mp3", "./assets/sounds/cowabunga.mp3", "./assets/sounds/hot-damn.mp3", "./assets/sounds/yas-queen", "./assets/sounds/ive-never-been-happier.mp3"];
+var incorrectSounds = ["./assets/sounds/holt-incredible-pain.mp3", "./assets/sounds/dirtbag.mp3", "./assets/sounds/everything-is-garbage.mp3", "./assets/sounds/holt-incredible-pain.mp3", "./assets/sounds/is-everything-ok.mp3", "./assets/sounds/painnnn.mp3", "./assets/sounds/terry-jeffords-why.mp3"];
+
+function randomSound(isCorrect) {
+    var tmpSound;
+
+    function randomIndex(arrayLength) {
+        return Math.floor(Math.random() * arrayLength);
+    }
+
+    if (isCorrect) {
+        tmpSound = new Audio(correctSounds[randomIndex(correctSounds.length)]);
+    } else {
+        tmpSound = new Audio(incorrectSounds[randomIndex(incorrectSounds.length)]);
+    }
+
+    tmpSound.play();
+
+}
+
 function displayQuestion(question) {
     //Display question picture
-    $("#questionPicture").attr("background-image", `url:${question.image}`);
+    $("#questionPicture").css("background-image", `url(${question.image})`);
     $("#question").text(question.text);
     // Clear question choice div
     $("#questionChoices").empty();
     // Render question choices
     for (choice of question.choices) {
-        var newChoice = $("<div class='questionChoice'>");
+        var newChoice = $("<div class='questionChoice activeChoice'>");
         choice.isAnswer ? newChoice.attr("id", "answer") : '';
         newChoice.text(choice.text);
         newChoice.appendTo("#questionChoices");
     }
     startTimer(question.duration);
 
-}
-// Find answer element with id="answer" and change styling (to green) 
-function showAnswer(question) {
-    $("#answer").addClass();
 }
 
 // Updates game stats
@@ -50,6 +66,47 @@ function pauseTimer() {
     }
 }
 
+function updateMessage(result) {
+    var gameMessage = $("#gameMessage");
+    if (result === "correct") {
+        gameMessage.text("Correct");
+        gameMessage.css({
+            "color": "rgb(75, 192, 75)",
+            "border": "5px solid rgb(75, 192, 75)"
+
+        });
+
+    } else if (result === "incorrect") {
+        gameMessage.text("Incorrect");
+        gameMessage.css({
+            "color": "rgb(250, 82, 82)",
+            "border": "5px solid rgb(250, 82, 82)",
+        });
+
+    } else if (result === "missed") {
+        gameMessage.text("Missed");
+        gameMessage.css({
+            "color": "rgb(250, 240, 107)",
+            "border": "5px solid rgb(250, 240, 107)",
+        });
+
+    }
+
+}
+
+function emptyMessage() {
+    var gameMessage = $("#gameMessage");
+    gameMessage.empty();
+    gameMessage.css({
+        "border": "0px solid white"
+
+    });
+}
+
+function showAnswer() {
+    $("#answer").addClass("correct");
+}
+
 function decrement() {
     time--;
     var formattedTime = timeConverter(time);
@@ -57,8 +114,19 @@ function decrement() {
 
     if (time === 0) {
         pauseTimer();
+        $(".questionChoice").removeClass("activeChoice");
+        updateMessage("missed");
         // show correct answer and move to next question
         showAnswer();
+        // play incorrect sound
+        randomSound(false);
+        questionIndex++;
+
+        nextQuestion();
+
+
+        // increment number of missed questions counter
+        numMissed++;
     }
 
 }
@@ -83,37 +151,65 @@ function timeConverter(t) {
 }
 
 // Restart game, reset stats
-function restartGame() {
+function newGame() {
     numCorrect = 0;
     numIncorrect = 0;
+    numMissed = 0;
     questionIndex = 0;
-    //
+    displayQuestion(questions[questionIndex]);
 }
 
-function endQuestion(isCorrect) {
+function endGame() {
+    //hide question and show end game message
+    setTimeout(function () {
+        $("#gameDisplay").empty();
+    }, 3000);
+    console.log("The game has ended");
 
-    if (isCorrect) {
-
-    } else {
-        // highlight user choice in RED
-        // highlight correct choice in GREEN
-        showAnswer();
-    }
-
-    updateStats(isCorrect);
-
-    if (questionIndex < questions.length) {
-        nextQuestion();
-    }
+    // show game stats
+    // show restart button
 }
 
 function nextQuestion() {
-    displayQuestion(question[++questionIndex]);
+    if (questionIndex < questions.length) {
+        setTimeout(function () {
+            emptyMessage();
+            displayQuestion(questions[questionIndex]);
+        }, 2500);
+
+    } else {
+        endGame();
+    }
 }
+
+function endQuestion(questionChoice) {
+    var isCorrect = (questionChoice.attr("id") === "answer");
+    randomSound(isCorrect);
+    $(".questionChoice").removeClass("activeChoice");
+    if (isCorrect) {
+        showAnswer();
+        updateMessage("correct");
+
+    } else {
+        // highlight user choice in RED
+        questionChoice.addClass("incorrect");
+        // highlight correct choice in GREEN
+        showAnswer();
+        updateMessage("incorrect");
+
+    }
+
+    updateStats(isCorrect);
+    questionIndex++;
+
+    nextQuestion();
+}
+
+
 
 $(document).ready(function () {
     $(function () {
-        var API = "https://api.myjson.com/bins/1fgobl";
+        var API = "https://api.myjson.com/bins/n2dxh";
         //RETRIEVE JSON DATA FROM API
         $.getJSON(API,
             function (q) {
@@ -132,12 +228,15 @@ $(document).ready(function () {
         $("#startButton").css("display", "none");
         //Show game display
         $("#gameDisplay").css("display", "block");
+        newGame();
+    });
 
-        displayQuestion(questions[questionIndex]);
-    })
-
-    $(".questionChoice").on("click", function () {
+    $(document.body).on("click", ".activeChoice", function () {
         pauseTimer();
-        endQuestion(($(this).attr("id") === "answer"));
-    })
+        endQuestion($(this));
+    });
+
+    $(document.body).on("click", "#restartButton", function () {
+        newGame();
+    });
 })
